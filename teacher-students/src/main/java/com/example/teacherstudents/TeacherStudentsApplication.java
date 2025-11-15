14b9631cb753da14da68b847689fbedb4c7fa6a7
Mohammed-Abdelghany <muhammed195840@feng.bu.edu.eg>
2025-11-15 23:25:02 +0200

    Initialize Spring Boot application with Maven wrapper and configuration files

---
+++ teacher-students/src/main/java/com/example/teacherstudents/TeacherStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.teacherstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherStudentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherStudentsApplication.class, args);
+    }
+
+}
---
+++ teacher-students/src/test/java/com/example/teacherstudents/TeacherStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teacherstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherStudentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
